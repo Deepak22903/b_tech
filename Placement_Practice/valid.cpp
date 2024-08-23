cfca80f4e505652959446d42062e7df3239c8e28
Deepak22903 <deepakshitole4@gmail.com>
2024-08-23 22:17:57 +0530

    valid.cpp

--- Placement_Practice/valid.cpp
+++ Placement_Practice/valid.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isValid(string s) { return true; }
+bool isValid(string s) {
+  stack<char> char_stack;
+  for (int i = 0; i < s.length(); i++) {
+    if (char_stack.empty()) {
+      char_stack.push(s[i]);
+      continue;
+    }
+    if (char_stack.top() == '(' && s[i] == ')') {
+      char_stack.pop();
+      continue;
+    }
+    if (char_stack.top() == '[' && s[i] == ']') {
+      char_stack.pop();
+      continue;
+    }
+    if (char_stack.top() == '{' && s[i] == '}') {
+      char_stack.pop();
+      continue;
+    }
+    char_stack.push(s[i]);
+  }
+  return char_stack.empty();
+}
 
-int main() { return 0; }
+int main() {
+  string s = "[]";
+  bool res = isValid(s);
+  cout << res << endl;
+  return 0;
+}
