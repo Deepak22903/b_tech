26fdb8b98f405959195a800a4f6fc3e2b0893b61
Deepak22903 <deepakshitole4@gmail.com>
2024-09-27 21:05:41 +0530

    Updated

--- PP/schedule_maker/main.cpp
+++ PP/schedule_maker/main.cpp
@@ -1,10 +1,4 @@
 #include "header.h"
 #include <iostream>
 
-int main() {
-  int choice = 0;
-  std::cout << "Select your choice >";
-  std::cin >> choice;
-
-  return 0;
-}
+int main() { return 0; }
