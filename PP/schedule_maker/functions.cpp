6d2da8cf475138a3570a32f8ba470894dafd1770
Deepak22903 <deepakshitole4@gmail.com>
2024-09-27 15:12:56 +0530

    new project

---
+++ PP/schedule_maker/functions.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+void select_choice() {
+  int choice = 0;
+  while (1) {
+    std::cout << "hi";
+  }
+  std::cout << "Select your choice >";
+  std::cin >> choice;
+}
---
+++ PP/schedule_maker/main.cpp
@@ -0,0 +1,10 @@
+#include "header.h"
+#include <iostream>
+
+int main() {
+  int choice = 0;
+  std::cout << "Select your choice >";
+  std::cin >> choice;
+
+  return 0;
+}
