9ee88096d2d0ed4a84681cdc3135ebb8c0e6e379
Deepak22903 <deepakshitole4@gmail.com>
2025-01-21 10:43:34 +0530

    Updated

--- Sem_6/OS/OS_Lab/Assignment_2_Shell/shell.c
+++ Sem_6/OS/OS_Lab/Assignment_2_Shell/shell.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-
-int main(){
-    while(1){
+int main() {
+    while (1) {
         char str[50];
         printf("prompt> ");
-        scanf("%s",str);
-        if(strcmp(str,"ls") == 0){
-            execlp("ls","ls",NULL);
+        scanf("%s", str);
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork failed");
+            return 1;
+        }
+
+        if (pid == 0) { 
+            if (strcmp(str, "ls") == 0) {
+                execlp("ls", "ls", NULL);
+            } else {
+                printf("Unknown command: %s\n", str);
+                return 1;
+            }
+        } else { 
+            wait(NULL); 
         }
     }
+    return 0;
 }
