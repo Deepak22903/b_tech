0f37e1f53899d53a772e12707d6ef722428849c4
Deepak22903 <deepakshitole4@gmail.com>
2025-02-05 17:11:12 +0530

    Updated

--- Sem_6/Mira_flows_workshop/test.py
+++ Sem_6/Mira_flows_workshop/test.py
@@ -1,6 +1,24 @@
 from dotenv import load_dotenv
-from mira_sdk import MiraClient
 import os
+from mira_sdk import MiraClient, Flow
 
 load_dotenv()
+
+# Initialize the client
 client = MiraClient(config={"API_KEY": os.getenv("MIRA_API_KEY")})
+
+version = "1.0.3"
+input_data = {
+    "blog_topic": "How to invest in stocks",
+    "content_type": "",
+    "audience_requirements": ""
+}
+
+# If no version is provided, latest version is used by default
+if version:
+    flow_name = f"@super-labs/blog-post-generator/{version}"
+else:
+    flow_name = "@super-labs/blog-post-generator"
+
+result = client.flow.execute(flow_name, input_data)
+print(result)
