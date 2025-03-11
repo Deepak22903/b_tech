32201ee0dbe5987e6c1b5a635ef30b6b11dc8584
Deepak22903 <deepakshitole4@gmail.com>
2025-03-11 10:18:12 +0530

    Updated

---
+++ Sem_6/OS/Project/test/sigint.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <signal.h>
+
+using namespace std;
+
+void handle_sig(int signo) { cout << "Signal Caught: " << signo << endl; }
+
+int main(int argc, char *argv[]) {
+  // raise(SIGTERM);
+  signal(SIGINT, handle_sig);
+  while (1)
+    ;
+  return 0;
+}
